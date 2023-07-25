90d7630de34ea13cf65c7eb5032d0a87e3394b01
pashasport <123305807+pashasport@users.noreply.github.com>
2023-07-25 23:38:37 +0300

    rating-popup

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+}
+
+const input = document.querySelector('.form-input');
+const modal = document.querySelector('.modal');
+const btnSend = document.querySelector('.submit-btn');
 const ratings = document.querySelectorAll('.rating');
 if (ratings.length > 0) {
   initRatings();
@@ -40,13 +56,52 @@ function initRatings() {
       });
       ratingItem.addEventListener('click', function (e) {
         initRatingVars(rating);
-        if (rating.dataset.ajax) {
-          setRatingValue(rating.value, rating);
-        } else {
-          ratingValue.innerHTML = index + 1;
-          setRatingActiveWidth();
-        }
+        // if (rating.dataset.ajax) {
+        //   setRatingValue(ratingItem.value, rating);
+        // } else {
+        ratingValue.innerHTML = index + 1;
+        setRatingActiveWidth();
+        // }
       });
     }
   }
+  // async function setRatingValue(value, rating) {
+  //   if (!rating.classList.add('rating-sending')) {
+  //     rating.classList.add('rating-sending');
+  //     let response = await fetch(
+  //       'https://tasty-treats-backend.p.goit.global/api/recipes/',
+  //       {
+  //         method: 'GET',
+  //         // body: JSON.stringify({
+  //         //   userRating: value,
+  //         // }),
+  //         // headers: {
+  //         //   'content-type': 'application/json',
+  //         // },
+  //       }
+  //     );
+  //     if (response.ok) {
+  //       const result = await response.json();
+  //       const newRating = result.newRating;
+  //       ratingValue.innerHTML = newRating;
+  //       setRatingActiveWidth();
+  //       rating.classList.remove('rating-sending');
+  //     } else {
+  //       alert('False');
+  //       PerformanceResourceTiming.classList.remove('rating-sending');
+  //     }
+  //   }
+  // }
 }
+const div = document.querySelector('.backdrop');
+function onBackdropClick(e) {
+  if (e.currentTarget !== e.target) {
+    div.classList.add('is-hidden');
+  }
+}
+document.addEventListener('keydown', function (e) {
+  if (e.code === 'Escape') {
+    // код клавиши Escape, но можно использовать e.key
+    div.classList.add('is-hidden');
+  }
+});
